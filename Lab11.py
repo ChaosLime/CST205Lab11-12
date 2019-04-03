4a1dc1e4ea225dec908738c2e94210a15d5606e6
Nick Saunders <nicholas3000@sbcglobal.net>
2019-04-03 09:47:37 -0700

    Inital commit, added Lab1*.py files with headers

---
+++ Lab11.py
@@ -0,0 +1,18 @@
+## CST 205 - Lab 11
+## Team #9 S.C.E.S.
+## Team Members:
+## Austin Ah Loo
+## Mikie Reed
+## Mitchell Saunders
+## Nicholas Saunders
+## Ramon Lucindo
+
+import os #to allow access to os.path.abspath(__file__) and os.path.dirname
+
+def setMediaPathToCurrentDir():
+  fullPathToFile = os.path.abspath(__file__)
+  if fullPathToFile.startswith('/'):
+    setMediaPath(os.path.dirname(fullPathToFile))
+  else:
+    setMediaPath(os.path.dirname(fullPathToFile) + '\\')
+
